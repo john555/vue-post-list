1c7b95e415bc3cf169a64dc0070e056b6124a425
John O. Paul <oyomthojohnpaul@gmail.com>
2019-07-03 14:06:28 +0300

    Bind PostList component to state

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -2,31 +2,44 @@
   <section class="post-list">
     <h2 class="post-list__title">Sortable Post List</h2>
     <ul class="post-list__list">
-      <post-list-item></post-list-item>
-      <post-list-item></post-list-item>
+      <post-list-item
+        v-for="(post, index) in posts"
+        :key="post.id"
+        :title="post.title"
+        :isFirstItem="index === 0"
+        :isLastItem="index === posts.length - 1"
+      ></post-list-item>
     </ul>
   </section>
 </template>
 <script>
+import { mapGetters, mapActions } from "vuex";
 import PostListItem from "./PostListItem.vue";
+
 export default {
   components: {
     PostListItem
+  },
+  methods: {
+    ...mapActions(["fetchPosts"])
+  },
+  computed: mapGetters(["posts"]),
+  mounted() {
+    this.fetchPosts();
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .post-list {
-}
+  &__title {
+    color: white;
+  }
 
-.post-list__title {
-  color: white;
-}
-
-.post-list__list {
-  padding: 0;
-  list-style: none;
-  margin: 0;
+  &__list {
+    padding: 0;
+    list-style: none;
+    margin: 0;
+  }
 }
 </style>
--- tests/unit/components/PostList.spec.js
+++ tests/unit/components/PostList.spec.js
@@ -1,9 +1,27 @@
+import Vue from "vue";
+import Vuex from "vuex";
 import { shallowMount } from "@vue/test-utils";
 import PostList from "@/components/PostList";
 
+Vue.use(Vuex);
+
 describe("components/PostList.vue", () => {
+  let store;
+
+  beforeEach(() => {
+    const state = {
+      posts: []
+    };
+    store = new Vuex.Store({
+      getters: {
+        posts: state => state.posts
+      },
+      state
+    });
+  });
+
   it("renders a vue instance", () => {
-    const wrapper = shallowMount(PostList);
+    const wrapper = shallowMount(PostList, { store });
     expect(wrapper.isVueInstance()).toBe(true);
   });
 });

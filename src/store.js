a084e4f3aceeba63fbacdef837e595d8bf00c7dd
John O. Paul <oyomthojohnpaul@gmail.com>
2019-07-03 14:05:56 +0300

    Add ability to fetch data from API

--- src/store.js
+++ src/store.js
@@ -1,10 +1,31 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
+
+import { VUE_APP_API_BASE_URL } from "./constants/api";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    posts: []
+  },
+  getters: {
+    posts: state => state.posts
+  },
+  mutations: {
+    setPosts(state, posts) {
+      state.posts = posts;
+    }
+  },
+  actions: {
+    async fetchPosts({ commit }) {
+      try {
+        const response = await axios.get(`${VUE_APP_API_BASE_URL}/posts`);
+        commit("setPosts", response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 });

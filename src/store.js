f9c483cabfd51d31dbd813f88931d33b9112538a
John O. Paul <oyomthojohnpaul@gmail.com>
2019-07-04 21:35:44 +0300

    Add caching for posts

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import Vuex from "vuex";
 import axios from "axios";
 
 import { VUE_APP_API_BASE_URL } from "./constants/api";
+import { LS_POSTS_KEY } from "./constants/localstorage";
 
 Vue.use(Vuex);
 
@@ -20,8 +21,15 @@ export default new Vuex.Store({
   },
   actions: {
     async fetchPosts({ commit }) {
+      const localData = localStorage.getItem(LS_POSTS_KEY);
+
+      if (localData) {
+        commit("setPosts", JSON.parse(localData));
+      }
+
       try {
         const response = await axios.get(`${VUE_APP_API_BASE_URL}/posts`);
+        localStorage.setItem(LS_POSTS_KEY, JSON.stringify(response.data));
         commit("setPosts", response.data);
       } catch (error) {
         console.error(error);
